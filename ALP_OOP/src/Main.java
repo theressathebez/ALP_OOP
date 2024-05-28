1f959a9746fadbcf6f7835df153e1b6d91fd7b9b
theressathebez <153163348+theressathebez@users.noreply.github.com>
2024-05-28 08:26:48 +0700

    test
    just testing

--- ALP_OOP/src/Main.java
+++ ALP_OOP/src/Main.java
@@ -10,7 +10,7 @@
 public class Main {
     public static void main(String[] args) {
         Appflow app = new Appflow();
-        
+        System.out.println("Hello there wassup");
         app.start();
     }
 }
